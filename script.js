c771381b1d71a60ab625ddea452f7f515c06a1ce
Megan Lavallee <megan.lavallee@yahoo.com>
2020-11-06 15:02:16 -0700

    submit

---
+++ script.js
@@ -0,0 +1,39 @@
+var lowercase = abcdefghijklmnopqrstuvwxyz
+var uppercase = abcdefghijklmnopqrstuvwxyz
+var numbers = 0123456789
+var specialCharacters = !@#$%^&*()-=+_. 
+
+promptCharacters("How Many Characters Would You Like?")
+promptLowercase("Do you want lower case letters?")
+promptUppercase("Do You Want uppercase letters?")
+promptSpecialcharacters("Would you like to add a special character?")
+promptNum("Do you want numbers?")
+
+
+
+ for (let lowerCase = 0; lowerCase < array.length; lowerCase++) {
+     const element = array[lowerCase];
+     
+ }
+
+ for (let uppercase = 0; uppercase < array.length; uppercase++) {
+     const element = array[uppercase];
+     
+ }
+
+ for (let number = 0; number < array.length; number++) {
+     const element = array[number];
+     
+ }
+
+ for (let specialCharacters = 0; specialCharacters < array.length; specialCharacters++) {
+     const element = array[specialCharacters];
+     
+ }
+ 
+ //<button id = "increament">Increament</button>
+//var button =document.querySelector("Increament")
+//Increament.addEventListener("click", handleIncreament)
+//function handleIncreament() {
+//count ++;
+//contelEL.textContent = count;
