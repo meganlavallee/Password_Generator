365be879df90f974c16413435a0939b63c8891a7
Megan Lavallee <megan.lavallee@yahoo.com>
2020-12-25 19:57:53 -0700

    buttons, array, checks

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Buttons
+const generateBtn = document.querySelector("#generate");
+const clearBtn = document.querySelector('#clear');
+const textArea = document.querySelector('#password');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Function for Generating Password
+function generatePassword() {
 
-  passwordText.value = password;
+  // Characters
+  const upperCharacter = document.querySelector("#upperCase");
+  const lowerCharacter = document.querySelector("#lowerCase");
+  const numbers = document.querySelector("#numbers");
+  const symbols = document.querySelector("#symbols");
+  
+  // Character Array
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const upperLetters = lowercaseLetters.toUpperCase();
+  const special = "!@#$%^&*()_+";
+  const numbs = "0123456789";
 
+  let character = "";
+
+  // Checks to see what buttons the user has pressed
+
+  // Upper Case Letters
+  if (upperCharacter.checked) {
+    character += upperLetters;
+  }
+  // Lower Case Letters
+  if (lowerCharacter.checked) {
+    character += lowerCaseLetters;
+  }
+
+  // Numbers
+  if (numbers.checked) {
+    character += numbs;
+  }
+
+  // Special Characters
+  if (symbols.checked) {
+    character += special;
+  }
+
+  
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
