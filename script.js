db0e639c3e3e327482ae58df1aaa09889ee0fe4f
Megan Lavallee <megan.lavallee@yahoo.com>
2020-12-28 15:23:23 -0700

    javascript

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // Buttons
 const generateBtn = document.querySelector("#generate");
 const clearBtn = document.querySelector("#clear");
+let length = document.getElementById("slider");
 
 // Areas on Main Screen
 const textArea = document.querySelector("#password");
+
+// Length
+const output = document.getElementById("demo");
+output.innerHTML = length.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+length.oninput = function () {
+  output.innerHTML = this.value;
+};
 
 // Function for Generating Password
 function generatePassword() {
@@ -12,12 +22,12 @@ function generatePassword() {
   const upperCharacter = document.querySelector("#upperCase");
   const lowerCharacter = document.querySelector("#lowerCase");
   const numbers = document.querySelector("#numbers");
-  const symbols = document.querySelector("symbols");
+  const special = document.querySelector("#special");
   
   // Character Array
   const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
   const upperLetters = lowercaseLetters.toUpperCase();
-  const special = "!@#$%^&*()_+";
+  const symbol = "!@#$%^&*()_+";
   const numbs = "0123456789";
   
   var character = "";
@@ -39,8 +49,8 @@ function generatePassword() {
   }
   
   // Special Characters
-  if (symbols.checked) {
-    character += special;
+  if (special.checked) {
+    character += symbol;
   }
   
   // Prompt For User to Double Check Button Pressing
@@ -48,30 +58,27 @@ function generatePassword() {
     alert("Please choose options for your password");
   } else {
     let password = "";
+    
     // For Loop
-    for (var i = 0; i < length; i++) {
+    for (var i = 0; i < length.value; i++) {
       password = password + character.charAt(Math.floor(Math.random() * character.length - 1));
       // const randonNumber = Math.floor(Math.random() * character.length);
       
       
       
       // password += character.charAt(randonNumber);
-      console.log(password);
     }
+    console.log(password);
+    document.querySelector("#password").value = password;
   }
   
-   document.querySelector("#password").textContent = password;
 }
 
-// Length
-let length = document.getElementById("length");
-const output = document.getElementById("demo");
-output.innerHTML = length.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-length.oninput = function () {
-  output.innerHTML = this.value;
-};
+function clearPassword () {
+  console.log("hello")
+  document.querySelector("#password").value = "";
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
+clearBtn.addEventListener("click", clearPassword);
